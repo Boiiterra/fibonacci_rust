894a7c75f424dd337600a0a517d665e630556e77
TerraBoii <kostyaborisenko05@gmail.com>
2022-08-05 14:41:14 +0300

    Does it work??

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
-pub fn fibonacci(n: u32) -> u32 {
-    return 0;
+pub fn fibonacci(n: u128) -> u128 {
+    if n == 1 {
+        return 0;
+    } else if n == 2 {
+        return 1;
+    } else if n == 3 {
+        return 1;
+    } else {
+        let mut previous: u128 = 1; // 2nd
+        let mut new: u128 = 1;  // 3rd
+        let mut ghost: u128;
+        let mut runner: u128 = 4;
+
+        while runner < n {
+            ghost = new;
+            new = previous + new;
+            previous = ghost;
+            runner += 1;
+        }
+
+        return new + previous;
+    }
 }
